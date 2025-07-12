5a44eeb87e6688f0af4a964f2424e106861c0be8
Samuel Campos <admdespachosca@gmail.com>
2025-07-11 18:24:55 -0700

    Update page.tsx

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -105,7 +105,7 @@ export default function ServiciosPage() {
       </section>
       
       {/* Services Cards Section */}
-      <section className="pt-8 pb-20 bg-white">
+      <section className="pt-0 pb-20 bg-white">
         <div className="container mx-auto px-4">
           <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
             {services.map((svc, idx) => (
