601e4d585fca1945d9b867ee57f99a02b84b01cf
Samuel Campos <admdespachosca@gmail.com>
2025-07-14 14:52:58 -0700

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -62,7 +62,7 @@ export default function ContactPage() {
             <h2 className="text-3xl md:text-4xl font-bold text-slate-900 mb-4 text-center">
               Ponte en contacto
             </h2>
-            <p className="text-xl text-slate-600 max-w-2xl mx-auto">
+            <p className="text-xl text-slate-600 max-w-2xl mx-auto text-center">
               Estamos aquí para apoyarte con todas sus necesidades contables y
               financieras. Contáctanos a través del método de su preferencia.
             </p>
