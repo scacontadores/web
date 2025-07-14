4740ac6893b611f19bf6ac626208d29662c9ddc7
Samuel Campos <admdespachosca@gmail.com>
2025-07-14 13:30:08 -0700

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -22,7 +22,7 @@ export default function NotFound() {
         </div>
 
             {/* Error Message */}
-        <div className="container mx-auto px-10 relative z-10">
+        <div className="container mx-auto px-8 relative z-10">
           <div className="max-w-4xl mx-auto text-center">
             <h1 className="text-4xl md:text-5xl font-bold mb-6">
               Página no encontrada
